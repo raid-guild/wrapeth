b8e12ec0617ff0c1008dfe32cee395c6aebcbe63
Scott Herren <me@stwoh.com>
2022-10-31 14:13:17 -0500

    transpile modules

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
+
+const withTM = require('next-transpile-modules')(['@raidguild/design-system']);
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig);
