afdafbfed8c5cb17f787c51ccbabd34cd73a24ef
nitegeist <nitegeist.dev@gmail.com>
2025-04-07 14:34:14 -0500

    minor tweak

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -82,6 +82,10 @@ const useFormField = () => {
     throw new Error('useFormField should be used within <FormField>');
   }
 
+  if (!itemContext) {
+    throw new Error('useFormField should be used within <FormItem>');
+  }
+
   const { id } = itemContext;
 
   return {
